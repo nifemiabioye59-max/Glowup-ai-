beb9d559850be29116229be2ccca4999e1ba721a
nifemiabioye59-max <nifemiabioye59@gmail.com>
2026-08-07 15:35:34 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from 'clsx'
+import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)) }
-export function formatPrice(amount: number): string {
-  return new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN', minimumFractionDigits: 0 }).format(amount / 100)
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
+
+export const ADMIN_EMAIL = 'nifemiabioye59@gmail.com'
+export const ADMIN_PASSWORD = 'jojoh$28crude$'
+
